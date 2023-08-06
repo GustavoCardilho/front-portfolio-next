391b89b5389de894cd62f74f36dd0b4b95c1ce11
Kyoudan <fogoste@gmail.com>
2023-08-06 18:03:14 -0300

    feat: creating more infomations

--- src/components/pages/home/services.tsx
+++ src/components/pages/home/services.tsx
@@ -1,4 +1,4 @@
-import { Briefcase, FileCode2 } from "lucide-react";
+import { Briefcase, FileCode2, Server } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
 
 export const ServicesHome = () => {
@@ -36,8 +36,23 @@ export const ServicesHome = () => {
             <h1>Disponivel para trabalhar</h1>
           </div>
           <p className="text-lg text-gray-500 font-montserrat font-bold text-justify">
-            Estou aceitando qualquer trabalho voltado a área de programação com
-            foco em back-end
+            Estou aceitando qualquer trabalho voltado a área de programação
+          </p>
+        </div>
+
+        <div
+          id="services3"
+          className="w-[450px] h-[400px] flex p-6 flex-col gap-2 bg-gradient-to-b from-black rounded border border-zinc-700 hover:bg-gradient-to-b hover:from-black hover:to-zinc-900 transition-all"
+        >
+          <div className="w-full flex-col gap-6 text-white font-montserrat font-bold text-2xl">
+            {" "}
+            <Server size={50} className="p-2 bg-zinc-900 rounded" />
+            <Separator className="my-6" color="#6b7280" />
+            <h1>Foco em back-end</h1>
+          </div>
+          <p className="text-lg text-gray-500 font-montserrat font-bold text-justify">
+            Tenho foco em back-end apesar de ter conhecimentos basicos em
+            front-end
           </p>
         </div>
       </div>
